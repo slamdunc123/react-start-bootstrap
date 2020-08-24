ccdc8a8a49f52487d41585e049975ed20fefc413
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-08-24 12:33:10 +0100

    fix root not found redirect issue on deployed

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Route1 from './components/Route1';
 import Route2 from './components/Route2';
 
 const Routes = () => {
 	return (
 		<Switch>
-			<Route path='/route1'>
+			<Route exact path='/'>
 				<Route1 />
 			</Route>
-			<Route path='/route2'>
+			<Route exact path='/route1'>
+				<Route1 />
+			</Route>
+			<Route exact path='/route2'>
 				<Route2 />
 			</Route>
+			<Route render={() => <Redirect to='/route1' />} />
+			{/* <Route component={Route1} /> */}
 		</Switch>
 	);
 };
