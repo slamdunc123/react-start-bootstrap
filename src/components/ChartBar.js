d22c2ceca163f519213961e07d8936227ee7a3ed
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-09-01 17:01:15 +0100

    change chart colour

--- src/components/ChartBar.js
+++ src/components/ChartBar.js
@@ -15,11 +15,11 @@ const ChartBar = () => {
 		datasets: [
 			{
 				label: 'My First dataset',
-				backgroundColor: 'rgba(255,99,132,0.2)',
-				borderColor: 'rgba(255,99,132,1)',
+				backgroundColor: 'rgba(97,218,251,0.2)',
+				borderColor: 'rgba(97,218,251,1)',
 				borderWidth: 1,
-				hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-				hoverBorderColor: 'rgba(255,99,132,1)',
+				hoverBackgroundColor: 'rgba(97,218,251,0.4)',
+				hoverBorderColor: 'rgba(97,218,251,1)',
 				data: [65, 59, 80, 81, 56, 55, 40],
 			},
 		],
